5e1802b680367272466738162b44f70467e1e88a
eric-deeporigin <edobroveanu@deeporigin.com>
2023-12-01 09:53:50 -0500

    day1 part2, fail RIP

--- day1/index.ts
+++ day1/index.ts
@@ -6,6 +6,54 @@ const input = fs
   .toString()
   .split("\n");
 
+const spelledOutDigits = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+const wordToDigit: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const lookAhead = (
+  index: number,
+  charArr: string[],
+  direction: number
+): string | undefined => {
+  const foundWord = spelledOutDigits.find((word) => {
+    const offsetIndex = index + direction * word.length;
+    let lookedAtWord = "";
+    // looking ahead
+    if (offsetIndex > index) {
+      lookedAtWord = charArr.slice(index, offsetIndex).join("");
+    } else {
+      // looking behind
+      lookedAtWord = charArr.slice(offsetIndex, index).join("");
+    }
+
+    return word === lookedAtWord;
+  });
+
+  if (foundWord) {
+    return String(wordToDigit[foundWord]);
+  }
+};
+
 const findDigit = (
   index: number,
   charArr: string[],
@@ -14,6 +62,11 @@ const findDigit = (
   const char = charArr[index];
   if (!Number.isNaN(Number(char))) {
     return char;
+  }
+
+  const wordDigit = lookAhead(index, charArr, direction);
+  if (wordDigit) {
+    return wordDigit;
   }
 
   return findDigit(index + direction, charArr, direction);
